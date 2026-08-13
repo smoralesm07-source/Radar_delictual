97a5b92f6e754086f4c0dc376b0acf74cb68b0a6
smoralesm07-source <smoralesm07@gmail.com>
2026-08-13 06:33:28 -0400

    test: retirar contrato v0.2 basado en homicidios

--- tests/test_v02_integration.py
+++ tests/test_v02_integration.py
@@ -1,9 +1,25 @@
 from radar_delictual.integration import build_integration_contract
 
-def test_contract_preserves_geographic_join_keys():
-    region=[{'region_code':'13','period':'2025-H1','territorial_priority_score':80}]
-    commune=[{'territory_id':'CL-13101','region_code':'13','commune_code':'13101','homicide_pressure_score':70}]
-    rows=build_integration_contract(region,commune)
-    assert rows[0]['join_keys']['region_code']=='13'
-    assert rows[1]['join_keys']['commune_code']=='13101'
-    assert rows[1]['aml_relevance']=='context_only'
+
+def test_contract_v03_preserves_geographic_join_keys_and_excludes_homicides():
+    """El contrato v0.2 fue sustituido: homicidios ya no son señal AML integrable."""
+    region = [{
+        'year': 2025,
+        'region_code': '13',
+        'pressure_score': 80,
+    }]
+    commune = [{
+        'territory_id': 'CL-13-santiago',
+        'period': '2024',
+        'region_code': '13',
+        'commune_code': '13101',
+        'commune_name_norm': 'santiago',
+        'cead_aml_priority_score': 70,
+        'article27_relation': 'direct_family_ley_20000',
+    }]
+    rows = build_integration_contract(region, commune)
+    assert rows[0]['join_keys']['region_code'] == '13'
+    assert rows[1]['join_keys']['commune_code'] == '13101'
+    assert rows[1]['aml_relevance'] == 'direct_predicate_family'
+    assert all(r['excludes_homicide_weight'] is True for r in rows)
+    assert all('homicid' not in ' '.join(r['source_families']).lower() for r in rows)
